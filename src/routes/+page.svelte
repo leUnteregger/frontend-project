e422395fd71758f9da00e06fd153c33e79b1acdc
Sint56 <sint.tobias@johak.at>
2024-11-22 11:21:35 +0100

    Sint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body class="flex justify-center bg-[#e2f1e7]">
+	<div
+		class="container mb-[100px] mt-[100px] flex h-[800px] w-1/2 flex-col rounded-[20px] bg-white p-[40px]"
+	>
+		<h1 class="mb-[20px] text-3xl font-bold">Contact Us</h1>
+		<p class="text-xs">First Name</p>
+	</div>
+</body>
